fdc952a67a212c37056686f2b4c864adf0c0635f
Colin <colinnlsn@gmail.com>
2016-10-31 19:42:29 -0500

    Added an Extension Method
    Added an extension method, LifestylePerOwinWebRequest that will modify
    the given BasedOnDescriptor and set the scope to
    OwinWebRequestScopeAccessor

---
+++ src/orth.windsor.owinlifestyle/BasedOnDescriptorLifestyleExtension.cs
@@ -0,0 +1,22 @@
+﻿using Castle.MicroKernel.Registration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orth.Windsor.Owinlifestyle
+{
+    public static class BasedOnDescriptorLifestyleExtension
+    {
+        /// <summary>
+        /// Use <see cref="OwinWebRequestScopeAccessor"/> as a Scoped Lifestyle
+        /// </summary>
+        /// <param name="descriptor">The Descriptor</param>
+        /// <returns></returns>
+        public static BasedOnDescriptor LifestylePerOwinWebRequest(this BasedOnDescriptor descriptor)
+        {
+            return descriptor.LifestyleScoped<OwinWebRequestScopeAccessor>();
+        }
+    }
+}
